fa279770b9484620320aa4078780b4aabef91a38
katleva <kot45363@gmail.com>
2025-09-24 10:10:23 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,34 @@
 import express from "express";
-import { authMiddleware } from './middleware/auth.js';
-import { logger } from './middleware/logger.js';
-import { errorHandler } from './middleware/errorHandler.js';
-import authRoutes from "./routes/authRoutes.js";
-import usersRoutes from "./routes/usersRoutes.js";
-import hostsRoutes from "./routes/hostsRoutes.js";
-import propertiesRoutes from './routes/propertiesRoutes.js';
-import bookingsRoutes from "./routes/bookingsRoutes.js";
-import reviewsRoutes from "./routes/reviewsRoutes.js";
+import { PrismaClient } from "@prisma/client";
+import usersRouter from "./routes/users.js";
+import bookingsRouter from "./routes/bookings.js";
+import propertiesRouter from "./routes/properties.js";
+import reviewsRouter from "./routes/reviews.js";
+import hostsRouter from "./routes/hosts.js";
+import authRouter from "./routes/login.js";
+import logMiddleware from "./middleware/logMiddleware.js";
+import errorHandler from "./middleware/errorHandler.js";
 
+
+const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
-app.use(logger);
-app.use((req, res, next) => {
-  if (
-    req.path === '/login' ||
-    (req.path === '/users' && req.method === 'POST')
-  ) {
-    return next();
-  }
-  authMiddleware(req, res, next);
-});
+app.use(logMiddleware);
 
+app.use("/login", authRouter);
 
-app.use('/login', authRoutes);
-app.use('/users', usersRoutes);
-app.use('/hosts', hostsRoutes);
-app.use('/properties', propertiesRoutes);
-app.use('/bookings', bookingsRoutes);
-app.use('/reviews', reviewsRoutes);
+app.use("/users", usersRouter);
+app.use("/bookings", bookingsRouter);   
+app.use("/properties", propertiesRouter); 
+app.use("/reviews", reviewsRouter);
+app.use("/hosts", hostsRouter);       
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
+
+
