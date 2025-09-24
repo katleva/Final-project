4a66e50ec5360cd9467133911665bcf7f78a7fe9
katleva <kot45363@gmail.com>
2025-09-24 10:17:40 +0200

    Delete src/middleware directory

--- src/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-export function logger(req, res, next) {
-  const start = Date.now();
-  res.on('finish', () => {
-    const time = Date.now() - start;
-    console.log(`${req.method} ${req.originalUrl} - ${time}ms`);
-  });
-  next();
-}
