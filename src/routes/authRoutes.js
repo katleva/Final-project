7b8fb5dc1ad2bc15c60c6649fcadca16e9d1a187
katleva <kot45363@gmail.com>
2025-09-24 10:12:25 +0200

    Delete src/routes/authRoutes.js

--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loginUser } from '../services/authService.js';
-
-const router = express.Router();
-
-// POST /login
-router.post('/', loginUser);
-
-export default router;
