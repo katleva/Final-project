9aa3042f8dcbc170a96f7d069aca18dbf4d52293
katleva <kot45363@gmail.com>
2025-09-29 07:27:13 +0200

    Update errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,9 @@
+import Sentry from '../instrument.js';
+
 const errorHandler = (err, req, res, next) => {
   console.error(err)
+  Sentry.captureException(err);
+
   res.status(500).json({ message: 'Something went wrong!' })
 }
 
